a8258700cdad423246c669c9eed991918b75fe9f
Yusuf Çepni <myusufcepni2@gmail.com>
2023-10-20 23:47:46 +0300

    nameSingelton da hata var Register3 sayfasında 20. satır ananı sikeyim

--- app/src/main/java/com/example/myapplication/registerScreen/Personalinformation.kt
+++ app/src/main/java/com/example/myapplication/registerScreen/Personalinformation.kt
@@ -1,20 +1,54 @@
 package com.example.myapplication.registerScreen
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.myapplication.R
+import com.example.myapplication.Singelton
+import com.example.myapplication.databinding.ActivityPersonalinformationBinding
 
 class Personalinformation : AppCompatActivity() {
+    private lateinit var binding: ActivityPersonalinformationBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_personalinformation)
-    }
-    fun register3(view : View){
-        val intent = Intent(this,Register3::class.java)
-        startActivity(intent)
-        finish()
+        binding = ActivityPersonalinformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
+    fun register3(view: View) {
+        val name = binding.name.text.toString()
+        val surname = binding.surname.text.toString()
+        val phoneNumber = binding.phoneNumber.text.toString()
+        val age = binding.age.text.toString()
+        val universityDepartment = binding.universityDepartment.text.toString()
+        val universityYear = binding.universityYears.text.toString()
+
+        if (name.isEmpty() || surname.isEmpty() || phoneNumber.isEmpty() || age.isEmpty() || universityDepartment.isEmpty() || universityYear.isEmpty()) {
+            showAlertDialog("Hata!", "Lütfen Alanları Doldurunuz")
+        } else {
+            // Alanlar doluysa kayıt işlemini başlat
+            Singelton.name = name
+            Singelton.phone = phoneNumber
+            Singelton.age = age
+            Singelton.universitydepartment = universityDepartment
+            Singelton.universityyear = universityYear
+
+            val intent = Intent(this,Register3::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun showAlertDialog(title: String, message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Tamam") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
 }
--- app/src/main/java/com/example/myapplication/registerScreen/Register3.kt
+++ app/src/main/java/com/example/myapplication/registerScreen/Register3.kt
@@ -1,20 +1,34 @@
-package com.example.myapplication.registerScreen
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.myapplication.R
+import com.example.myapplication.Singelton
+import com.example.myapplication.databinding.ActivityRegister3Binding
 import com.example.myapplication.loginScreen.LoginPage
 
 class Register3 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegister3Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register3)
+        binding = ActivityRegister3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val singletonName = Singelton.name
+        if (singletonName != null) {
+            binding.nameSingelton.text = singletonName
+        } else {
+            // Singleton'daki 'name' özelliği null ise, ekrandaki öğeye bir varsayılan değer atayabilirsiniz veya atama işlemini atlayabilirsiniz.
+            // Örnek: binding.nameSingelton.text = "Varsayılan Değer"
+        }
+
+        // EditText öğesini düzenlenebilir hale getirin
+        binding.nameSingelton.isEnabled = true
     }
 
     fun goSuccessPage(view: View) {
-        val intent = Intent(this,LoginPage::class.java)
+        val intent = Intent(this, LoginPage::class.java)
         startActivity(intent)
     }
 }
