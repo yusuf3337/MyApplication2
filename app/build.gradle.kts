f17fdac0c7b1279cb4089a619c2ec18aa7d00903
Yusuf Çepni <myusufcepni2@gmail.com>
2023-10-27 22:52:59 +0300

    SallerHomeSingelton eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.27")
     implementation("com.google.android.material:material:1.0.0-beta01")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/myapplication/Singelton2.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+object SallerHomeSingelton {
+    var ilanBasligi : String? = null
+    var fiyat : String? = null
+    var m2 : String? = null
+    var odaSayisi : String? = null
+    var binaYasi : String? = null
+    var banyoSayisi : String? = null
+    var balkon : String? = null
+    var esyali : String? = null
+
+    var katSayisi : String? = null
+    var bulunduguKat : String? = null
+    var kullanimDurumu : String? = null
+    var cephe : String? = null
+    var ulasim : String? = null
+    var isitma : String? = null
+    var ogrenciyeUygun : String? = null
+    var ilanAciklamasi : String? = null
+
+    var acikAdres : String? = null
+
+    var evGoremeyeIzinVarmi : String? = null
+    var xKonum : Double? = null
+    var yKonum : Double? = null
+
+    var kategori : String? = null
+}
