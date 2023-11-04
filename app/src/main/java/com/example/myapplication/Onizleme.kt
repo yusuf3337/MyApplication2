e1c5295009f0c5e90c57f5fdc02c5e9f84d9f8d2
Yusuf Çepni <myusufcepni2@gmail.com>
2023-11-04 03:08:16 +0200

    Fragmentler den 2 tanesi düzenlendi home_Screen ufaktan ayarlandı box ve textler düzenlendi size,dp,sp ayarlandı

---
+++ app/src/main/java/com/example/myapplication/Onizleme.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onizleme : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onizleme)
+    }
+}
