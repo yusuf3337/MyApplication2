33756a1f68c0d26af577ecfb64ced1d11aedcf72
Yusuf Çepni <myusufcepni2@gmail.com>
2023-12-14 21:43:03 +0200

    Günlük Kiralık ev sayfalarının tasarımları yapıldı

---
+++ app/src/main/java/com/example/myapplication/gunlukKiralikEv2.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gunlukKiralikEv2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gunluk_kiralik_ev2)
+    }
+}
